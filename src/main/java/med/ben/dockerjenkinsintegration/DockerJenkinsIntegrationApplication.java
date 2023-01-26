c150393fa8c8d9253eacaedf7d76d4e50f60e6fd
Mohamedben01 <mohamedbendahui7@gmail.com>
2023-01-26 23:12:28 +0100

    Add new method

--- src/main/java/med/ben/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
+++ src/main/java/med/ben/dockerjenkinsintegration/DockerJenkinsIntegrationApplication.java
@@ -6,6 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerJenkinsIntegrationApplication {
 
+	public void message(){
+		System.out.println("Hello World!");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(DockerJenkinsIntegrationApplication.class, args);
 	}
